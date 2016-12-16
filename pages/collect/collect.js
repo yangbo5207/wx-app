1cd6e9bf383029dbe1cb9130fc10e157e3829ff7
yangbo <yangbo@tigerbrokers.com>
2016-12-16 17:13:36 +0800

    收藏页

---
+++ pages/collect/collect.js
@@ -0,0 +1,44 @@
+import config from '../../utils/config'
+import state from '../../utils/state'
+import { promise } from '../../utils/utils'
+
+Page({
+    data: {
+        currentTab: 0,
+        recommendList: []
+    },
+    onLoad: function () {
+        const _this = this
+        const authorization = state.get('authorization')
+        promise(wx.request)({
+            url: `${config.communityDomainDev}/v5/user/favorites`,
+            header: { Authorization: authorization },
+            data: {
+                pageCount: 1,
+                pageSize: 20
+            }
+        })
+        .then( result => {
+            console.log(result)
+            _this.setData({
+                recommendList: result.data.data
+            })
+        })
+    },
+    bindChange: function (e) {
+        this.setData({
+            currentTab: e.detail.current
+        })
+    },
+    switchNav: function (e) {
+        let self = this
+
+        if(this.data.currentTab === e.target.dataset.current) {
+            return false;
+        } else {
+            self.setData({
+                currentTab: e.target.dataset.current
+            })
+        }
+    }
+}) 
