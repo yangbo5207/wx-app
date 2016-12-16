d96725c5a2e711cffd9acefd39ead13800faf6ee
yangbo <yangbo@tigerbrokers.com>
2016-12-16 16:05:16 +0800

    修改昵称

---
+++ pages/nickname/nickname.js
@@ -0,0 +1,74 @@
+import config from '../../utils/config'
+import state from '../../utils/state'
+import { promise } from '../../utils/utils'
+
+Page({
+    data: {
+        nickname: '',
+        inputContent: ''
+    },
+    onLoad () {
+        let author = state.get('author')
+        this.setData({
+            nickname: author.name
+        }) 
+    },
+    input (event) {
+        this.setData({
+            inputContent: event.detail.value
+        })
+    },
+    saveNickname () {
+        const _this = this
+        const authorization = state.get('authorization')
+
+        if(_this.data.inputContent == '') {
+            wx.showModal({
+                title: '提示',
+                content: '请输入您要修改的名称'
+            })
+
+            return;
+        }
+
+        promise(wx.request)({
+            url: `${config.communityDomainDev}/v5/user`,
+            header: { 
+                Authorization: authorization,
+                'content-type': 'application/x-www-form-urlencoded;charset=UTF-8' 
+            },
+            method: 'PUT',
+            data: {
+                name: _this.data.inputContent
+            }
+        })
+        .then( result => {
+            const _name = result.data.data.name
+            _this.setData({
+                nickname: _name
+            })
+            state.set({
+                author: {
+                    name: _name
+                }
+            })
+            wx.showToast({
+                title: `昵称已经修改为${_name}`,
+                icon: 'success',
+                duration: 1000
+            })
+            setTimeout( () => {
+                wx.navigateBack({
+                    delta: 1
+                })
+            }, 1000)
+            
+        })
+        .catch( result => {
+            wx.showModal({
+                title: '提示',
+                content: result.data.message
+            })
+        })
+    }
+})
--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -1,3 +1,23 @@
+import config from '../../utils/config'
+import state from '../../utils/state'
+import { promise } from '../../utils/utils'
+
 Page({
-    
+    data: {
+        nickname: ''
+    },
+    onLoad () {
+        const _this = this
+        _this.setData({
+            nickname: state.get('author').name
+        })
+    },
+    onShow () {
+        this.onLoad()
+    },
+    navToNickname () {
+        wx.navigateTo({
+            url: '../nickname/nickname'
+        })
+    },
 })
