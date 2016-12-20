f3b3bebab7a5f1429428f13d76697bf98ceddc81
yangbo <yangbo@tigerbrokers.com>
2016-12-20 19:52:01 +0800

    update

--- pages/bindphone/step01/step01.js
+++ pages/bindphone/step01/step01.js
@@ -1,14 +1,57 @@
 import { http } from '../../../utils/utils'
 import state from '../../../utils/state'
+import config from '../../../utils/config'
 
 Page({
-    data: {},
+    data: {
+        inputValue: ''
+    },
     onLoad () {
 
     },
-    testBack () {
-        wx.navigateTo({
-            url: state.get('sourcePage')
+    input (event) {
+        this.setData({
+            inputValue: event.detail.value
+        })
+    },
+    nextStep () {
+        if (this.data.inputValue.length < 11 || !/^1[3|4|5|7|8][0-9]{9}$/.test(this.data.inputValue)) {
+            wx.showModal({
+                title: '提示',
+                content: '请输入正确的手机号'
+            })
+            return
+        }
+        
+        http(wx.request)({
+            url: `${config.loginDomain}/api/v4/auth/sns/snsbind/status`,
+            data: { username: this.data.inputValue }
+        })
+        .then(result => {
+            if (result.data.phone == null) {
+                console.log('该手机号未注册，跳转至注册页面')
+                state.set({
+                    bindingPhone: this.data.inputValue
+                })
+                wx.redirectTo({
+                    url: '../step02/step02'
+                })
+            }
+            if (result.data.phone == this.data.inputValue && !result.data.sns_status.wxapp_bingding) {
+                console.log('该手机号已注册，但是未绑定小程序，跳转至绑定页面')
+                state.set({
+                    bindingPhone: this.data.inputValue
+                })
+                wx.redirectTo({
+                    url: '../step12/step12'
+                })
+            }
+            if (result.data.phone == this.data.inputValue && result.data.sns_status.wxapp_bingding) {
+                wx.showModal({
+                    title: '提示',
+                    content: '该手机号已经绑定了其他小程序'
+                })
+            }
         })
     }
 })
--- pages/bindphone/step02/step02.js
+++ pages/bindphone/step02/step02.js
@@ -1,8 +1,18 @@
+import { http } from '../../../utils/utils'
+import state from '../../../utils/state'
+import config from '../../../utils/config'
+
 Page({
     data: {
-        isEncryption: 1
+        isEncryption: 1,
+        inputValue: ''
     },
-    onLoad () {},
+    onLoad () {
+        this.setData({
+            inputValue: state.get('bindingPhone')
+        })
+        
+    },
     switchEncryption () {
         this.setData({
             isEncryption: !this.data.isEncryption
