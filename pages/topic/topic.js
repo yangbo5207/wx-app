b7570678a88685d64dc669acfcdfa8fe66423537
yangbo <yangbo@tigerbrokers.com>
2016-12-20 16:07:19 +0800

    主题页下拉刷新

--- pages/topic/topic.js
+++ pages/topic/topic.js
@@ -1,41 +1,68 @@
 import config from '../../utils/config'
 import state from '../../utils/state'
-import { promise } from '../../utils/utils'
+import { http } from '../../utils/utils'
+import { getData } from '../../components/pullDownRefresh/pullDownRefresh'
 
 const app = getApp()
 
 Page({
     data: {
         topicInfo: {},
-        topicList: []
+        topicList: [],
+        windowWidth: 0,
+        windowHeight: 0
     },
     onLoad () {
         // const postid = state.get('postid')
         const postid = 42
         const authorization = state.get('authorization')
 
-        promise(wx.request)({
+        http(wx.getSystemInfo)()
+        .then(result => {
+            this.setData({
+                windowWidth: result.windowWidth,
+                windowHeight: result.windowHeight -110
+            })
+        })
+
+        http(wx.request)({
             url: `${config.communityDomainDev}/v5/topic/${postid}`,
             header: {
                 'Authorization': authorization
             }
-        }) 
+        })
         .then( result => {
             this.setData({
                 topicInfo: result.data
             })
         })
 
-        promise(wx.request)({
-            url: `${config.communityDomainDev}/v5/topic/${postid}/tweets`,
-            header: {
-                'Authorization': authorization
-            }
-        })
-        .then( result => {
-            this.setData({
-                topicList: result.data
-            })
+        this.getTopics(true)
+    },
+    getTopics (boolean) {
+        const authorization = state.get('authorization')
+        // const postid = state.get('postid')
+        const postid = 42
+        const _topic = `${config.communityDomainDev}/v5/topic/${postid}/tweets`
+        const pageSize = 10
+
+        if (boolean) {
+            return getData(this, {
+                url: _topic,
+                data: {
+                    pageCount: 1,
+                    pageSize: pageSize
+                },
+                header: { 'Authorization': authorization }
+            }, true)
+        }
+        return getData(this, {
+            url: _topic,
+            data: {
+                pageCount: this.data.pageCount + 1,
+                pageSize: pageSize
+            },
+            header: { 'Authorization': authorization }
         })
     },
     navToPost(event) {
@@ -47,4 +74,7 @@ Page({
             url: '../post/post'
         })
     },
+    upLoad () {
+        this.data.enablePullDownRefresh && this.getTopics()
+    }
 })
