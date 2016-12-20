1ad875c3314395fd7428bede8aa201dcc8c073ca
yangbo <yangbo@tigerbrokers.com>
2016-12-20 12:27:07 +0800

    下拉刷新组件

--- components/pullDownRefresh/pullDownRefresh.js
+++ components/pullDownRefresh/pullDownRefresh.js
@@ -1,1 +1,100 @@
-import { promise } from '../../utils/utils'
+import { http } from '../../utils/utils'
+import state from '../../utils/state'
+
+/**
+ * 需要保存在当前页面对象data中的变量如下
+ * dataList [] 列表的数据，该组件只负责将数据保存在data中，渲染样式需要在page中重新定义
+ * isData 1|0 是否有数据  
+ * isBottomLoading 1|0 是否显示下拉刷新的loading提示
+ * isBottomEnd 1|0 是否到底了
+ * enablePullDownRefresh 1|0 是否还能下拉刷新
+ * pageCount {number} 表示第几页
+ */
+
+/**
+ * @desc 请求列表数据，如果不是第一次请求，则为下拉刷新
+ * @param {object} _this 页面对象
+ * @param {object} options wx.request请求需要的参数对象,
+ * @param {boolean} isFirst 是否是第一次请求数据
+ * @return 返回promise对象
+ */
+function get (_this, options, isFirst) {
+    const authorization = state.get('authorization')
+
+    if (isFirst) {
+        wx.showToast({
+            title: '加载中...',
+            icon: 'loading',
+            duration: 10000
+        })
+        _this.setData({
+            dataList: [],
+            isData: 1,
+            isBottomLoading: 0,
+            isBottomEnd: 0,
+            enablePullDownRefresh: 1,
+            pageCount: 1
+        })
+    } else {
+        if (!_this.data.enablePullDownRefresh) {return }
+        _this.setData({
+            isBottomLoading: 1,
+            pageCount: _this.data.pageCount + 1
+        })
+    }
+
+    return http(wx.request)(options)
+    .then( result => {
+        if (result.data) {
+            if (isFirst) {
+                delayHideToast()
+                _this.setData({
+                    dataList: result.data
+                })
+            } else {
+                this.setData({
+                    dataList: _this.data.dataList.concat(result.data),
+                    isBottomLoading: 0
+                })
+            }
+        } 
+        if (result.message) {
+            _this.setData({
+                isBottomLoading: 0,
+                isBottomEnd: 1,
+                enablePullDownRefresh: 0
+            })
+        }
+    })
+    .catch (result => {
+        delayHideToast()
+        if (result.status) {
+            http(wx.showModal)({
+                title: '提示',
+                content: `错误码：${result.status}`,
+                confirmText: '重新加载'
+            })
+            .then(result => {
+                if (result.confirm) {
+                    get (_this, options, isFirst)
+                }
+            })
+        } else {
+            wx.showModal({
+                title: '提示',
+                content: '网络好像出了点问题'
+            })
+        }
+    })
+}
+
+function delayHideToast () {
+    let timer = setTimeout ( () => {
+        wx.hideToast()
+        clearTimeout(timer)
+    }, 500)
+}
+
+module.exports = {
+    getData: get
+}
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,18 +1,14 @@
 import config from '../../utils/config'
 import state from '../../utils/state'
 import { promise } from '../../utils/utils'
+import { getData } from '../../components/pullDownRefresh/pullDownRefresh'
 
 const app = getApp()
 
 Page({
     data: {
-        recommendList: [],
-        isBottomLoading: "none",
-        isBottomEnd: "none",
         windowWidth: 0,
         windowHeight: 0,
-        pageCount: 1,  // 数据的页码
-        enable: 1      // 是否还能下拉刷新
     },
     onLoad () {
         const authorization = state.get('authorization')
@@ -74,88 +70,30 @@ Page({
     },
     getRecommendList (boolean) {
         const authorization = state.get('authorization')
-        let pageCount;
-
-        if(!this.data.enable) {
-            return;
+        const _streamings = `${config.communityDomainDev}/v5/streamings`
+        const pageSize = 10
+        if (boolean) {
+            return getData(this, {
+                url: _streamings,
+                data: {
+                    pageCount: 1,
+                    pageSize: pageSize
+                },
+                header: {
+                    'Authorization': authorization
+                }
+            }, true)
         }
-
-        if (boolean === true) {
-            wx.showToast({
-                title: '加载中...',
-                icon: 'loading',
-                duration: 100000
-            })
-            pageCount = 1;
-            this.setData({
-                pageCount: pageCount
-            })
-        } else {
-            this.setData({
-                isBottomLoading: 'flex',
-                pageCount: this.data.pageCount + 1
-            })
-        }
-
-        promise(wx.request)({
-            url: `${config.communityDomainDev}/v5/streamings`,
+        return getData (this, {
+            url: _streamings,
             data: {
-                pageCount: this.data.pageCount,
-                pageSize: 10
+                pageCount: this.data.pageCount + 1,
+                pageSize: pageSize
             },
             header: {
                 'Authorization': authorization
             }
         })
-        .then(result => {
-            // if(result.statusCode == 200) {
-            if(result.data) {
-                if(boolean === true) {
-                    delayHideToast()
-                    this.setData({
-                        recommendList: result.data
-                    })
-                } else {
-                    this.setData({
-                        recommendList: this.data.recommendList.concat(result.data),
-                        isBottomLoading: 'none'
-                    })
-                }
-            } else if (result.message) {
-                this.setData({
-                    isBottomLoading: 'none',
-                    isBottomEnd: 'flex',
-                    enable: 0
-                })
-            }
-            console.log('index页面加载完成')
-        }, result => {
-            delayHideToast();
-            console.log('index feed error message:', result)
-            if (result.status) {
-                promise(wx.showModal)({
-                    title: "提示",
-                    content: "错误码:" + result.status,
-                    confirmText: "重新加载"
-                })
-                .then(res => {
-                    if(res.confirm) {
-                        this.getRecommendList();
-                    }
-                })
-            } else {
-                wx.showModal({
-                    title: "提示",
-                    content: "未连接到服务器，请检测是否为网络问题"
-                })
-            }
-        })
-
-        function delayHideToast() {
-            setTimeout( () => {
-                wx.hideToast()
-            }, 500)
-        }
     },
     // 下拉加载更多
     upLoad () {
