2bd2d0353c4bb39354a056826651605677348d53
yangbo <yangbo@tigerbrokers.com>
2016-12-23 15:43:20 +0800

    update

--- utils/config.js
+++ utils/config.js
@@ -2,6 +2,10 @@ const config = {
     oauth: 'https://test-oauth.tigerbrokers.com',
     community: 'https://test-frontend-community.laohu8.com',
     news: 'https://stock-news-dev.tigerbrokers.com'
+
+    // oauth: 'https://oauth.tigerbrokers.com',
+    // community: 'https://frontend-community.laohu8.com',
+    // news: 'https://stock-news.tigerbrokers.com'
 }
 
 module.exports = config
